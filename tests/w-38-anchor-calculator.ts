6c0f09ec5a4fb719d9fad04f56f941fd9f835d27
Vedik Saini <vediksaini0704@gmail.com>
2025-06-09 15:09:29 +0530

    tests for calculator program

--- tests/w-38-anchor-calculator.ts
+++ tests/w-38-anchor-calculator.ts
@@ -1,16 +1,120 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { W38AnchorCalculator } from "../target/types/w_38_anchor_calculator";
+import { AnchorCal } from "../target/types/anchor_cal";
+import { it } from "mocha";
+import { assert } from "chai";
 
-describe("w-38-anchor-calculator", () => {
+describe("anchor-calculator", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.w38AnchorCalculator as Program<W38AnchorCalculator>;
+  const program = anchor.workspace.anchorCal as Program<AnchorCal>;
+  const dataAccount = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Is initialised", async () => {
+    const tx = await program.methods
+      .init(10)
+      .accounts({
+        account: dataAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .signers([dataAccount])
+      .rpc();
+    console.log("Transaction signature", tx);
   });
+
+  it("Is addition ", async () => {
+    const tx = await program.methods
+      .add(10)
+      .accounts({
+        account: dataAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .rpc();
+
+    console.log("Transaction signature", tx);
+    const account = await program.account.dataShape.fetch(dataAccount.publicKey);
+    assert.equal(account.num, 20);
+  });
+
+  it("Is subtraction ", async () => {
+    const tx = await program.methods
+      .sub(10)
+      .accounts({
+        account: dataAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .rpc();
+
+    console.log("Transaction signature", tx);
+    const account = await program.account.dataShape.fetch(
+      dataAccount.publicKey
+    );
+    assert.equal(account.num, 10);
+  });
+   
+  it("Is multiply ", async () => {
+    const tx = await program.methods
+      .mul(2)
+      .accounts({
+        account: dataAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .rpc();
+
+    console.log("Transaction signature", tx);
+    const account = await program.account.dataShape.fetch(
+      dataAccount.publicKey
+    );
+    assert.equal(account.num, 20);
+  });
+
+  it("Is division ", async () => {
+    const tx = await program.methods
+      .div(5)
+      .accounts({
+        account: dataAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .rpc();
+
+    console.log("Transaction signature", tx);
+    const account = await program.account.dataShape.fetch(
+      dataAccount.publicKey
+    );
+    assert.equal(account.num, 4);
+  });
+
+  it("Is double ", async () => {
+    const tx = await program.methods
+      .double()
+      .accounts({
+        account: dataAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .rpc();
+
+    console.log("Transaction signature", tx);
+    const account = await program.account.dataShape.fetch(
+      dataAccount.publicKey
+    );
+    assert.equal(account.num, 8);
+  });
+
+  it("Is half ", async () => {
+    const tx = await program.methods
+      .half()
+      .accounts({
+        account: dataAccount.publicKey,
+        signer: anchor.getProvider().wallet.publicKey,
+      })
+      .rpc();
+
+    console.log("Transaction signature", tx);
+    const account = await program.account.dataShape.fetch(
+      dataAccount.publicKey
+    );
+    assert.equal(account.num, 4);
+  });
+
 });
